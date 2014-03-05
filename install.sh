7cea8714ff6b8f558110c31bdbe4492bbd5fdb3a
Bastian Schulte <b_schulte@me.com>
2014-03-05 18:35:52 +0100

    reverted modifications
    this has to be done manually because of permissions

--- install.sh
+++ install.sh
@@ -3,6 +3,3 @@
 # install requirements
 apt-get install binfmt-support qemu qemu-user-static debootstrap kpartx lvm2 dosfstools apt-cacher-ng coreutils
 
-# creating rcopy folder
-BASEDIR='pwd'
-mkdir $BASEDIR\delivery\rcopy
