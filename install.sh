aa4c5aa21fa323feb6a8b6fdc10c605913eb9476
Bastian Schulte <bastian.schulte@me.com>
2014-03-05 17:40:58 +0100

    added rcopy creation

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+# install requirements
 apt-get install binfmt-support qemu qemu-user-static debootstrap kpartx lvm2 dosfstools apt-cacher-ng coreutils
+
+# creating rcopy folder
+BASEDIR='pwd'
+mkdir $BASEDIR\delivery\rcopy
